5c8d54b961fc36f7cc0c04e546fb2b5bdd557b6c
Hudson Pereira <hudson.byte@gmail.com>
2016-03-09 15:40:30 -0300

    ajusting facebook webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,18 +1,25 @@
 class WebhookController < ApplicationController
   def get
-    save_and_print_params
+    save_and_print_challenge
+
+    @challenge = params['hub.challenge']
+
+    render layout: false
   end
 
   def post
-    save_and_print_params
+    save_and_print_challenge
+
+    @challenge = params['hub.challenge']
+
+    render layout: false
   end
 
   def list
     @webhooks = Webhook.order(created_at: 'DESC')
   end
 
-  def save_and_print_params
-  	Webhook.create({payload: params});
-  	render json: params
+  def save_and_print_challenge
+    Webhook.create({payload: params});
   end
 end
